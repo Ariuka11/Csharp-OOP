21616cde225a8fc3ba9a2a1ea9635bb772e4df41
Ariunbold Oyungerel <ariuka11@Ariunbolds-MacBook-Pro.local>
2023-01-03 14:38:17 -0500

    Created StopWatch

---
+++ StopWatch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+
+            for (var i=0; i<2; i++)
+            {
+                stopWatch.Start();
+                Thread.Sleep(10000);
+                stopWatch.Stop();
+
+                Console.WriteLine("Duration:  " + stopWatch.GetInterval());
+                Console.WriteLine("Press Enter to run it again...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,33 @@
+﻿namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private bool _running;
+
+        public void Start()
+        {
+            if (_running)
+                throw new InvalidOperationException("StopWatch is already running!");
+            
+            _startTime = DateTime.Now;
+            _running = true;
+        }
+
+        public void Stop()
+        {
+            if (!_running)
+                throw new InvalidOperationException("StopWatch is not started!");
+
+            _stopTime = DateTime.Now;
+            _running = false;
+        }
+
+        public TimeSpan GetInterval()
+        {
+            return _stopTime - _startTime;
+        }
+    }
+}
+
