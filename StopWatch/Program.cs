cdddfe7b658edb66b1da21f72309117d9b011c0b
Ariunbold Oyungerel <ariuka11@Ariunbolds-MacBook-Pro.local>
2023-01-03 15:38:40 -0500

    Test

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 
 namespace StopWatch
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var stopWatch = new StopWatch();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var stopWatch = new StopWatch();
 
-            for (var i=0; i<2; i++)
-            {
-                stopWatch.Start();
-                Thread.Sleep(10000);
-                stopWatch.Stop();
+			for (var i=0; i<2; i++)
+			{
+				stopWatch.Start();
+				Thread.Sleep(10000);
+				stopWatch.Stop();
 
-                Console.WriteLine("Duration:  " + stopWatch.GetInterval());
-                Console.WriteLine("Press Enter to run it again...");
-                Console.ReadLine();
-            }
-        }
-    }
+				Console.WriteLine("Duration:  " + stopWatch.GetInterval());
+				Console.WriteLine("Press Enter to run it again...");
+				Console.ReadLine();
+			}
+		}
+	}
 }
 
